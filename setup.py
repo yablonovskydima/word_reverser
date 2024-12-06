39ffa7d99c68cb1361cfc09e7e077a29fbcd28ef
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-12-06 04:33:37 +0200

    modified setup

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='reverse_words_package',
+    name='dima_yablonovskyi_reverse_words_package',
     version='0.1',
     packages=find_packages(),
     test_suite='tests',
     description='A package for reversing letters in words while preserving the position of special symbols and numbers.',
     author='Dmytro Yablonovskyi',
     author_email='yablonovskydmutro1@gmail.com',
-    url='https://github.com/yablonovskydima/word_reverser.git'
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yablonovskydima/word_reverser',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    test_suite='reverse_words_package.test_reverse_words'
 )
