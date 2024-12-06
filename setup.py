5ce60dd011e5f9d4d810a60541d1287969d5eec1
yablonovskydima <yablonovskydmutro1@gmail.com>
2024-12-06 04:11:20 +0200

    modified setup

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(
     name='reverse_words_package',
     version='0.1',
     packages=find_packages(),
-     test_suite='tests',
+    test_suite='tests',
     description='A package for reversing letters in words while preserving the position of special symbols and numbers.',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Dmytro Yablonovskyi',
+    author_email='yablonovskydmutro1@gmail.com',
+    url='https://github.com/yablonovskydima/word_reverser.git'
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
